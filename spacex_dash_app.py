2b783938cdb13b1e2da942e7164b383f807645c0
Adegbuyi M Adeyemi <89105963+gbouhi1@users.noreply.github.com>
2021-11-29 21:23:19 -0500

    Update spacex_dash_app.py

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -15,36 +15,25 @@ min_payload = spacex_df['Payload Mass (kg)'].min()
 app = dash.Dash(__name__)
 
 # Create an app layout
-app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
-                                        style={'textAlign': 'center', 'color': '#503D36',
-                                               'font-size': 40}),
+app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard', style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
                                 # TASK 1: Add a dropdown list to enable Launch Site selection
                                 # The default select value is for ALL sites
                                 # dcc.Dropdown(id='site-dropdown',...)
                                 dcc.Dropdown(id='site-dropdown',
                                 options=[{'label':'All SITES','value':'ALL'},
-                                        {'label':'CCAFS LC-40','value':'CCAFS LC-40'},
-                                        {'label':'VAFB SLC-4E','value':'VAFB SLC-4E'},
-                                        {'label':'KSC LC-39A','value':'KSC LC-39A'},
-                                        {'label':'CCAFS SLC-40','value':'CCAFS SLC-40'}],
+                                        {'label':'CCAFS LC-40','value':'CCAFS LC-40'},{'label':'VAFB SLC-4E','value':'VAFB SLC-4E'},
+                                        {'label':'KSC LC-39A','value':'KSC LC-39A'}, {'label':'CCAFS SLC-40','value':'CCAFS SLC-40'}],
                                 value='ALL',
                                 placeholder='Select a Launch Site here',
                                 searchable=True),
 
-
-
                                 html.Br(),
-
-
 
                                 # TASK 2: Add a pie chart to show the total successful launches count for all sites
                                 # If a specific launch site was selected, show the Success vs. Failed counts for the site
                                 html.Div(dcc.Graph(id='success-pie-chart')),
 
-
                                 html.Br(),
-
-
 
                                 html.P("Payload range (Kg):"),
                                 # TASK 3: Add a slider to select payload range
@@ -55,8 +44,6 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                     marks = {0:'0',2500:'2500', 5000:'5000',7500:'7500',10000:'10000'},
                                     value=[min_payload, max_payload]
                                 ),
-
-
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
